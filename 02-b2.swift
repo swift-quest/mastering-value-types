966a77c77a5d52a0a5ddc61486ee5c2bc358cebc
Yuta Koshizawa <koher@koherent.org>
2018-05-15 10:18:50 +0900

    Add 02-b2

---
+++ 02-b2.swift
@@ -0,0 +1,61 @@
+class Character {
+    let name: String // 名前
+    let hp: Int      // HP
+    let mp: Int      // MP
+    let attack: Int  // 攻撃力
+    let defense: Int // 防御力
+
+    init(name: String, hp: Int, mp: Int, attack: Int, defense: Int) {
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.attack = attack
+        self.defense = defense
+    }
+}
+
+extension Character {
+    func copy(hp: Int? = nil, mp: Int? = nil) -> Character {
+        return Character(
+            name: self.name,
+            hp: hp ?? self.hp,
+            mp: mp ?? self.mp,
+            attack: self.attack,
+            defense: self.defense
+        )
+    }
+}
+
+func performAttack(by character: Character, to target: Character) -> Character {
+    var target = target
+
+    print("\(character.name)のこうげき。")
+
+    let damage = Swift.max(0, (character.attack - target.defense) / 2)
+    target = target.copy(hp: target.hp - damage)
+
+    print("\(target.name)に\(damage)のダメージ！")
+    print()
+
+    return target
+}
+
+func main() {
+    var hero = Character(name: "ゆうしゃ", hp: 153, mp: 25, attack: 162, defense: 97)
+    var archfiend = Character(name: "まおう", hp: 999, mp: 99, attack: 185, defense: 58)
+
+    print(hero.name)
+    print("HP \(hero.hp)")
+    print("MP \(hero.mp)")
+    print()
+
+    archfiend = performAttack(by: hero, to: archfiend)
+    hero = performAttack(by: archfiend, to: hero)
+
+    print(hero.name)
+    print("HP \(hero.hp)")
+    print("MP \(hero.mp)")
+    print()
+}
+
+main()
