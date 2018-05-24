c36cf54fc7ea547f62d630aefc035fc8bbcc64c3
Yuta Koshizawa <koher@koherent.org>
2018-05-24 14:49:28 +0900

    Upate 08-a

--- 08-a.swift
+++ 08-a.swift
@@ -1,16 +1,21 @@
 class Character {
     let name: String // 名前
+    let maxHP: Int   // 最大HP
     var hp: Int      // HP
+    let maxMP: Int   // 最大MP
     var mp: Int      // MP
     let attack: Int  // 攻撃力
     let defense: Int // 防御力
 
-    init(name: String, hp: Int, mp: Int, attack: Int, defense: Int) {
+    init(name: String, maxHP: Int, maxMP: Int, attack: Int, defense: Int) {
         self.name = name
-        self.hp = hp
-        self.mp = mp
+        self.maxHP = maxHP
+        self.maxMP = maxMP
         self.attack = attack
         self.defense = defense
+
+        self.hp = maxHP
+        self.mp = maxMP
     }
 }
 
@@ -28,18 +33,28 @@ extension Party {
     }
 }
 
-class Scene {
-    let friendParty: Party
-    let enemyParty: Party
+enum Spell {
+    case attack(name: String, mp: Int, damage: Int)
+    case restoration(name: String, mp: Int, quantity: Int)
 
-    init(friendParty: Party, enemyParty: Party) {
-        self.friendParty = friendParty
-        self.enemyParty = enemyParty
+    var name: String {
+        switch self {
+        case .attack(let name, _, _): return name
+        case .restoration(let name, _, _): return name
+        }
     }
 
-    func performRound() {
-        // TODO
+    var mp: Int {
+        switch self {
+        case .attack(_, let mp, _): return mp
+        case .restoration(_, let mp, _): return mp
+        }
     }
+}
+
+extension Spell {
+    static let fireball: Spell = .attack(name: "ファイアボール", mp: 5, damage: 70)
+    static let healing: Spell = .restoration(name: "ヒーリング", mp: 5, quantity: 80)
 }
 
 func performAttack(by character: Character, to target: Character) {
@@ -47,38 +62,61 @@ func performAttack(by character: Character, to target: Character) {
 
     let damage = Swift.max(0, (character.attack - target.defense) / 2)
     target.hp -= damage
-    character.hp += damage
 
     print("\(target.name)に\(damage)のダメージ！")
-    print("\(character.name)のHPがかいふくした。")
     print()
 }
 
-func main() {
-    let scene = Scene(
-        friendParty: Party(members: [
-            Character(name: "ゆうしゃ", hp: 153, mp: 25, attack: 162, defense: 97),
-            Character(name: "せんし", hp: 198, mp: 0, attack: 178, defense: 111),
-            Character(name: "そうりょ", hp: 101, mp: 35, attack: 76, defense: 55),
-            Character(name: "まほうつかい", hp: 77, mp: 58, attack: 60, defense: 57),
-        ]),
-        enemyParty: Party(members: [
-            Character(name: "まおう", hp: 999, mp: 99, attack: 185, defense: 58),
-            Character(name: "あんこくきし", hp: 250, mp: 0, attack: 181, defense: 93),
-            Character(name: "デモンプリースト", hp: 180, mp: 99, attack: 121, defense: 55),
-        ])
-    )
+func performSpell(_ spell: Spell, by character: Character, to target: Character) {
+    defer { print() }
 
-    for character in scene.friendParty.members {
+    print("\(character.name)は\(spell.name)のまほうをつかった。")
+
+    guard character.mp >= spell.mp else {
+        print("しかしMPがたりない。")
+        return
+    }
+
+    character.mp -= spell.mp
+
+    switch spell {
+    case .attack(_, _, let damage):
+        target.hp -= damage
+        print("\(target.name)に\(damage)のダメージ！")
+    }
+}
+
+func main() {
+    let friendParty = Party(members: [
+        Character(name: "ゆうしゃ", maxHP: 153, maxMP: 25, attack: 162, defense: 97),
+        Character(name: "せんし", maxHP: 198, maxMP: 0, attack: 178, defense: 111),
+        Character(name: "そうりょ", maxHP: 101, maxMP: 35, attack: 76, defense: 55),
+        Character(name: "まほうつかい", maxHP: 77, maxMP: 58, attack: 60, defense: 57),
+    ])
+    let enemyParty = Party(members: [
+        Character(name: "まおう", maxHP: 999, maxMP: 99, attack: 185, defense: 58),
+        Character(name: "あんこくきし", maxHP: 250, maxMP: 0, attack: 181, defense: 93),
+        Character(name: "デモンプリースト", maxHP: 180, maxMP: 99, attack: 121, defense: 55),
+    ])
+
+    friendParty.leader.hp = 1
+
+    for character in friendParty.members {
         print(character.name)
         print("HP \(character.hp)")
         print("MP \(character.mp)")
         print()
     }
 
-    // TODO
+    for character in friendParty.members {
+        performSpell(.fireball, by: character, to: enemyParty.leader)
+    }
 
-    for character in scene.friendParty.members {
+    for character in enemyParty.members {
+        performSpell(.fireball, by: character, to: friendParty.leader)
+    }
+
+    for character in friendParty.members {
         print(character.name)
         print("HP \(character.hp)")
         print("MP \(character.mp)")
